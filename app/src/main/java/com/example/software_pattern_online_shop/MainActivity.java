b791213fd1ccdcdcf63cf432491447b9f386db95
RachelRice13 <ricerachel12@gmail.com>
2023-04-01 14:44:21 +0100

    Added Landing Page

--- app/src/main/java/com/example/software_pattern_online_shop/MainActivity.java
+++ app/src/main/java/com/example/software_pattern_online_shop/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.software_pattern_online_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.software_pattern_online_shop.LoginRegister.LoginActivity;
+import com.example.software_pattern_online_shop.LoginRegister.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Go to Register Page
+        Button goToRegisterPage = findViewById(R.id.register_button);
+        goToRegisterPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        // Go to Login Page
+        Button goToLoginPage = findViewById(R.id.login_button);
+        goToLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        // Hides the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
