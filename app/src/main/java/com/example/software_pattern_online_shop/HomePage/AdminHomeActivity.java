35ee21a28c9fbc42a71b436ad676d8b5aecb659e
RachelRice13 <ricerachel12@gmail.com>
2023-04-03 16:22:47 +0100

    Added logout functionality for customers and admin

--- app/src/main/java/com/example/software_pattern_online_shop/HomePage/AdminHomeActivity.java
+++ app/src/main/java/com/example/software_pattern_online_shop/HomePage/AdminHomeActivity.java
@@ -1,29 +1,36 @@
 package com.example.software_pattern_online_shop.HomePage;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.software_pattern_online_shop.Customer.CustomerDetailsFragment;
+import com.example.software_pattern_online_shop.MainActivity;
 import com.example.software_pattern_online_shop.R;
 import com.example.software_pattern_online_shop.Stock.AddNewStockFragment;
 import com.example.software_pattern_online_shop.Stock.UpdateStockFragment;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminHomeActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         buildMenu();
 
         // Hides the action bar
@@ -67,10 +74,37 @@ public class AdminHomeActivity extends AppCompatActivity {
                         drawerLayout.close();
                         return true;
                     case R.id.nav_customer_logout:
+                        logout();
                         return true;
                 }
                 return false;
             }
         });
+    }
+
+    private void logout() {
+        AlertDialog.Builder logoutAD = new AlertDialog.Builder(AdminHomeActivity.this);
+
+        logoutAD
+                .setCancelable(false)
+                .setTitle("Do you want to logout?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        firebaseAuth.signOut();
+                        Intent logoutIntent = new Intent(AdminHomeActivity.this, MainActivity.class);
+                        startActivity(logoutIntent);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        drawerLayout.close();
+                    }
+                });
+
+        AlertDialog alertDialog = logoutAD.create();
+        alertDialog.show();
     }
 }
