9d218207d049fbb3fc1ee2cea8960633ba709e55
RachelRice13 <ricerachel12@gmail.com>
2023-04-03 16:12:06 +0100

    Added navigation functionality for customers and admin

--- app/src/main/java/com/example/software_pattern_online_shop/HomePage/AdminHomeActivity.java
+++ app/src/main/java/com/example/software_pattern_online_shop/HomePage/AdminHomeActivity.java
@@ -1,16 +1,76 @@
 package com.example.software_pattern_online_shop.HomePage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.example.software_pattern_online_shop.Customer.CustomerDetailsFragment;
 import com.example.software_pattern_online_shop.R;
+import com.example.software_pattern_online_shop.Stock.AddNewStockFragment;
+import com.example.software_pattern_online_shop.Stock.UpdateStockFragment;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class AdminHomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        buildMenu();
+
+        // Hides the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+
+    private void buildMenu() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new AdminHomeFragment()).commit();
+        MaterialToolbar toolbar = findViewById(R.id.admin_top_app_bar);
+        drawerLayout = findViewById(R.id.admin_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.admin_navigation_view);
+        drawerLayout.bringToFront();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_admin_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new AdminHomeFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_admin_add_new_stock:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new AddNewStockFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_admin_update_stock:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new UpdateStockFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_admin_customer_details:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CustomerDetailsFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_customer_logout:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/software_pattern_online_shop/HomePage/CustomerHomeActivity.java
+++ app/src/main/java/com/example/software_pattern_online_shop/HomePage/CustomerHomeActivity.java
@@ -1,16 +1,71 @@
 package com.example.software_pattern_online_shop.HomePage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.example.software_pattern_online_shop.Basket.BasketFragment;
+import com.example.software_pattern_online_shop.Profile.ProfileFragment;
 import com.example.software_pattern_online_shop.R;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class CustomerHomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+
+        buildMenu();
+
+        // Hides the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+
+    private void buildMenu() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CustomerHomeFragment()).commit();
+        MaterialToolbar toolbar = findViewById(R.id.customer_top_app_bar);
+        drawerLayout = findViewById(R.id.customer_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.customer_navigation_view);
+        drawerLayout.bringToFront();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_customer_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CustomerHomeFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_customer_basket:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new BasketFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_customer_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ProfileFragment()).commit();
+                        drawerLayout.close();
+                        return true;
+                    case R.id.nav_customer_logout:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
